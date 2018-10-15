c82324984e4500ea9c7eb63e2f1714930976561c
Gunjan Shakya <gr.skya@gmail.com>
2018-10-15 00:33:23 -0500

    goat addition added

--- src/movement.js
+++ src/movement.js
@@ -1,5 +1,31 @@
+var isTigerTurn = false;
+var goatCount =20;
+var goatDropCount = 1;
+
+
+window.onload = function(){
+  var squares = document.getElementsByClassName('Square');
+  for(var i = 0; i < squares.length; i++) {
+      var square = squares[i];
+      if(isEmpty(square)){
+        square.onclick = function (e){
+          var goat = goatElement();
+          e.target.appendChild(goat)
+        }
+      }       
+  }  ;
+}
+
 function allowDrop(ev) {
   ev.preventDefault();
+}
+
+function goatElement(){
+  var elem = document.createElement("img");
+  elem.setAttribute("src", "img/goat.png");
+  elem.setAttribute("class", "goat");
+  elem.setAttribute("id", "goat"+ goatDropCount);
+  return elem;
 }
 
 function drag(ev) {
@@ -10,26 +36,26 @@ function drag(ev) {
 }
 
 function enableTravelableSquareOf(squareId) {
-  activateDroppablesquare(+squareId + +1)
-  activateDroppablesquare(+squareId + +10)
-  activateDroppablesquare(squareId - 1)
-  activateDroppablesquare(squareId - 10)
+  activateDroppableSquare(+squareId + +1)
+  activateDroppableSquare(+squareId + +10)
+  activateDroppableSquare(squareId - 1)
+  activateDroppableSquare(squareId - 10)
   var omniDirectionPostitons = ["11", "13", "15", "22", "24", "31", "33", "35", "42", "44", "51", "53", "55"]
   if (omniDirectionPostitons.includes(squareId)) {
-    activateDroppablesquare(+squareId + +11)
-    activateDroppablesquare(squareId - 11)
-    activateDroppablesquare(+squareId + +9)
-    activateDroppablesquare(squareId - 9)
+    activateDroppableSquare(+squareId + +11)
+    activateDroppableSquare(squareId - 11)
+    activateDroppableSquare(+squareId + +9)
+    activateDroppableSquare(squareId - 9)
   }
 }
 
-function activateDroppablesquare(squareId) {
+function activateDroppableSquare(squareId) {
 
   var square = document.getElementById(squareId)
   console.log("checking: " + squareId + "=" + square)
   if (square) {
-    console.log(squareId + " occupied : " + isOccupied(square))
-    if (isOccupied(square)) {
+    console.log(squareId + " occupied : " + isEmpty(square))
+    if (isEmpty(square)) {
 
       console.log(squareId + ': valid')
       square.setAttribute("ondrop", "drop(event)")
@@ -47,8 +73,8 @@ function deactivateAllDroppable() {
   }
 }
 
-function isOccupied(square) {
-  return !square.innerHTML.trim() === ""
+function isEmpty(square) {
+  return square.innerHTML.trim() === ""
 }
 
 function drop(ev) {
